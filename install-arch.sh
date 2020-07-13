fa9b04640f65e99bc9de6298b0c16a50951c155e
Norbytus <norbyt93@gmail.com>
2020-07-13 08:44:19 +0300

    Delete curl request

--- install-arch.sh
+++ install-arch.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sudo pacman -S xclip nodejs ctags npm rubygems python2-pip python-pip
 yay -S ibexo liberation-fonts-ttf global
