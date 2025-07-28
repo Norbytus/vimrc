7470d833aafdb1418ba0d4ac7b95edd62c200516
Aleksandr Startsev <a.startsev@apicworld.com>
2025-07-28 10:25:53 +0300

    feat: Setup toggle-term

--- init.lua
+++ init.lua
@@ -56,3 +56,4 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.splitright = true
--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,3 +1,19 @@
 return {
-	'akinsho/toggleterm.nvim', version = "*", config = true
+    'akinsho/toggleterm.nvim', version = "*", config = true,
+    cmd = {
+        "ToggleTerm",
+        "TermExec",
+        "ToggleTermToggleAll",
+        "ToggleTermSendCurrentLine",
+        "ToggleTermSendVisualLines",
+        "ToggleTermSendVisualSelection",
+    },
+    config = function()
+        require("toggleterm").setup({
+            direction = "float",
+        })
+    end,
+    keys = {
+        { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" }
+    },
 }
